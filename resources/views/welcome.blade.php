ec541e4cf32aeca11ca4f13c20924cd73b64ab1d
MohitP197 <paudel823@gmail.com>
2019-02-20 11:39:39 +0545

    Various components were created.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div id="example"></div>
-<script src="{{asset('js/app.js')}}" ></script>
+<div class="container">
+    <div id="example"></div>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
